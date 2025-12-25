51892315cbd8001ae4a60ff0b69c47c0d09191c3
[dyad] <jarvis@bravetux.com>
2025-12-25 22:45:25 +0530

    [dyad] Fixing router context error by adding BrowserRouter to App.tsx - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
 import SIPPage from './fedu/SIPPage';
 import STPPage from './fedu/STPPage';
@@ -10,13 +10,15 @@ import BondsPage from './pages/BondsPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/finance-education/sip" element={<SIPPage />} />
-      <Route path="/finance-education/stp" element={<STPPage />} />
-      <Route path="/finance-education/swp" element={<SWPPage />} />
-      <Route path="/money-minds/bonds" element={<BondsPage />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/finance-education/sip" element={<SIPPage />} />
+        <Route path="/finance-education/stp" element={<STPPage />} />
+        <Route path="/finance-education/swp" element={<SWPPage />} />
+        <Route path="/money-minds/bonds" element={<BondsPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
